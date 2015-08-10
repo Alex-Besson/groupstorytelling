9c5b90c91cbb0f59cff4c523c98e5225d08d366a
Miwand <miwand.najafe@gmail.com>
2015-08-10 17:24:51 -0400

    Added a third sentence

--- GroupStorytellingPlayground.playground/Contents.swift
+++ GroupStorytellingPlayground.playground/Contents.swift
@@ -4,3 +4,4 @@ import UIKit
 
 var firstStorySentence = "In the ancient land of Eye Ohess there was a problem."
 var secondSentence = "The new software update caused more problems then it was looking to fix."
+var thirdSentence = "The truth was the software was made to cause problems in the first place"
